1b4180d859f7107ba77b6f8081a3edbd0e63fa53
root <root@ip-172-31-43-222.eu-west-1.compute.internal>
2023-04-25 17:47:06 +0000

    reworked routing on index

--- index.php
+++ index.php
@@ -7,12 +7,10 @@ define('VIEW_DIR', __DIR__.'/views');
 define('MODEL_DIR', __DIR__.'/models');
 define('CONTROLLER_DIR', __DIR__.'/controllers');
 
+if (isset($_REQUEST['route'])) {
+    require_once("controllers/{$_REQUEST['route']}.php");
+    exit;
+}
+
 require_once(__DIR__ . "/controllers/index.php");
-die();
-if (!isset($_POST['route'])) 
-    die( json_encode( ['error' => true, 'msg' =>'no route specified'] ) );
     
-if (!file_exists("controllers/{$_POST['route']}.php"))
-    die( json_encode( ['err'=>true, 'msg' => 'route not found'] ) );
-    
-require_once("controllers/{$_POST['route']}.php");
